0112fc7296397dee680bae156d19344fe110775c
gcpaccori <ptic252@else.com.pe>
2026-02-17 14:51:17 -0500

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    // serverActions: { ... }  <-- BORRAR esta sección completa
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
