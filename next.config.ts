fa000572f08a88ece6be3b0a257f6ce6e3014418
gcpaccori <ptic252@else.com.pe>
2026-02-17 14:48:05 -0500

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    serverActions: {
-      allowedOrigins: ["localhost:3000", "*.app.github.dev"],
-    },
+    serverActions: false,
   },
 };
 
